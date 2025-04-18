c0b71c406087b6812d61256b65a2500ddfd58648
Raymond Jessop <rjessop@zeeksgeeks.com>
2025-04-18 14:41:37 -0500

    update to apps.py

--- django_aws_ses/apps.py
+++ django_aws_ses/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class DjangoAwsSesBackendConfig(AppConfig):
+    """Configuration for the Django AWS SES email backend app."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'django_aws_ses'
-    verbose_name = 'Django AWS SES'
+    verbose_name = 'AWS SES Email Backend'
